3555984f93437371ba83289b543f93aa3bcda536
A--G <A--G@users.noreply.github.com>
2014-11-11 14:56:32 +0100

    Praktikum 11.11.2014

--- charfinder/src/charfinder/Charfinder.java
+++ charfinder/src/charfinder/Charfinder.java
@@ -12,6 +12,8 @@ public class Charfinder {
 		cf.printAllCharPositions(s);
 		cf.getCharPos(s, 'p');
 		
+		cf.countCharOccAlaWhileLoop(s, 'k');
+		cf.countCharOccAlaForLoop(s, 'k');
 		//char t = s.charAt(0);
 		//System.out.println(s + " length is") + s.length();	
 		//char l = s.charAt(s.length()-1);
@@ -37,7 +39,8 @@ public class Charfinder {
 		}
 	}
 	
-	void getCharPos(String s, Character toFind) {
+	public void getCharPos(String s, Character toFind) 
+	{
 		s = s.toLowerCase();
 		
 		Integer currentPosition = 0;
@@ -45,8 +48,10 @@ public class Charfinder {
 		
 		Boolean found = false; 
 		
-		while (currentPosition <= lastPosition){
-			if (s.charAt(currentPosition) == toFind) {
+		while (currentPosition <= lastPosition)
+		{
+			if (s.charAt(currentPosition) == toFind) 
+			{
 				found = true;
 				System.out.println(toFind + " is at position " + currentPosition + " of " + s);
 			}
@@ -59,11 +64,73 @@ public class Charfinder {
 			System.out.println(toFind + " was not found");
 		}
 	}
+	
+	public void countCharOccAlaForLoop(String s, Character toFind) 
+	{
+		
+		s = s.toLowerCase();
+		Integer stringLength = s.length();
+		Integer counter = 0;
+		
+		for(Integer i = 0; i < stringLength; i++) 
+		{
+			if(s.charAt(i) != toFind) 
+			{
+				continue;
+			}
+			else 
+			{
+				counter++;
+			}
+			
+			if(counter == 0) {
+				System.out.println(" " + toFind + " was not found in " + s + ".");
+			}
+		} 
+			if(counter == 1) {
+				System.out.println("There is " + counter + " occurence of " + toFind + " in " + s);
+			} else {
+				System.out.println("There are  " + counter + " occurrences of " + toFind + " in " + s + "." );
+			}
+			
+	}
+	
+	//Mail an mondaca schicken
+	
+	public void countCharOccAlaWhileLoop(String s, Character toFind) 
+	{		
+			s = s.toLowerCase();
+			Integer counter = 0;
+			Integer currentPosition = 0;
+			
+			while(currentPosition < s.length()) 
+			{
+				
+				if(s.charAt(currentPosition) == toFind) 
+				{
+					counter++;
+				}
+				
+				if(counter == 0) 
+				{
+					System.out.println(" " + toFind + " was not found in " + s + ".");
+				}
+				
+				currentPosition++;
+			} 
+				if(counter == 1) 
+				{
+					System.out.println("There is " + counter + " occurence of " + toFind + " in " + s);
+				} 
+				else 
+				{
+					System.out.println("There are  " + counter + " occurrences of " + toFind + " in " + s + "." );
+				}			
+		}
 }
 
 
-
-
+// for (Initialisierung, Abbruchbedingung, Inkrementierung){Anweisung}
 
 
 
